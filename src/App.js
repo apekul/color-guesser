e9937b32eb3d0f6b03f67846881b0d23029238ab
Adaś <adampekul@gmail.com>
2022-11-30 02:23:12 +0100

    Added function randomRGB that generates random rgb color, and basic quiz like functionality(when correct button with color name is clicked console will display WIN else LOOSE)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import "./style.css";
+import React, { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [list, setList] = useState([]);
+  const [answer, setAnswer] = useState();
+
+  function randomRGB() {
+    let obj = { r: 0, g: 0, b: 0 };
+    Object.keys(obj).map((v) => (obj[v] = Math.floor(Math.random() * 255)));
+    return "rgb(" + obj.r + "," + obj.g + "," + obj.b + ")";
+  }
+
+  const check = (e) => {
+    return e === answer ? console.log("WIN") : console.log("LOOSE");
+  };
+
+  useEffect(() => {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+      arr.push(randomRGB());
+    }
+    setList(arr);
+    setAnswer(arr[Math.floor(Math.random() * 3)]);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="group">
+        <div
+          className="display"
+          id="test"
+          style={{
+            backgroundColor: answer,
+          }}
+        >
+          ????
+        </div>
+        <div className="buttons">
+          {list.map((v, i) => (
+            <button value={v} onClick={(e) => check(e.target.value)} key={i}>
+              {v}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
